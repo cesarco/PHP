770bcd0907629fbd1b3c32e627de9c57aa1fa9e4
cesarco <87877289+Pcesarco@users.noreply.github.com>
2021-12-05 15:57:30 -0500

    Comentarios PHP

--- Ejercicio1.php
+++ Ejercicio1.php
@@ -1,6 +1,13 @@
 <?php 
 
+// Instrucción de impresión de informacion
+
 echo "Hello world, from villavicencio";
+
+/*
+Comentario largoDB::delete('delete users where name = ?', ['John'])
+Este es un comentario de código
+*/
 
 print_r(" hola mundo");
 
