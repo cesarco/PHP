c696935fb7cc3029009eb82f8c97d138fca14a86
cesarco <87877289+Pcesarco@users.noreply.github.com>
2021-12-05 21:20:04 -0500

    Constantes | PHP

---
+++ Ejercicio_7.php
@@ -0,0 +1,20 @@
+<?php
+
+$edad = 32; // Por ser una variable podemos cambiar el valor, como ejemplo la linea 3 y 7
+
+echo $edad. "<br/>";
+
+$edad = 40;
+
+echo $edad."<br>";
+
+// Uso de constante
+define("NOMBRE", "Cesar");
+
+// imprimir constante
+// algo diferente a lo ya visto, en las constantes no se utiliza el signo pesos
+ echo NOMBRE;
+
+
+
+?>
