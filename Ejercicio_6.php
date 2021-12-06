ddcc80c7ab2ce7b0026ab63ac58bf715f9665737
cesarco <87877289+Pcesarco@users.noreply.github.com>
2021-12-05 21:10:01 -0500

    Tipos de datos | PHP

---
+++ Ejercicio_6.php
@@ -0,0 +1,13 @@
+<?php
+
+$Edad = 32; // Entero
+
+$nombre = "Cesar"; //String
+
+$Altura = 1.75; // Flotante o decimal
+
+$Programador=null; // valor nulo
+
+echo "El nombre es ".$nombre. ", la edad es  ".$Edad.", su altura es ".$Altura.", el puesto ".$Programador;
+
+?>
