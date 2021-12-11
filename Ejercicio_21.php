69475e4ae32918ec294327df5ab4f55fada33dfb
cesarco <87877289+Pcesarco@users.noreply.github.com>
2021-12-11 16:49:40 -0500

    Arreglos de indice númerico | PHP

---
+++ Ejercicio_21.php
@@ -0,0 +1,12 @@
+<?php
+
+// los arreglos son una forma de guardar informacion
+
+// los valores que estan destro del array es un indice, que comienza desde cero
+
+
+$frutas = array("fresa","Pera","manzana");
+
+print_r($frutas);
+
+?>
