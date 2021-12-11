4e7c21693b8e11d09fc29a9b6d7ca42738e6466b
cesarco <87877289+Pcesarco@users.noreply.github.com>
2021-12-11 17:56:21 -0500

    Clases | PHP

---
+++ Ejercicio_24.php
@@ -0,0 +1,18 @@
+<?php
+
+class persona {
+
+    public $nombre; // propiedades
+
+    public function asignarNombre($nuevoNombre){ // acciones o metodos..
+
+            $this->nombre=$nuevoNombre;
+    }
+}
+
+$objetoAlumno = new persona(); // instacion p creacion de un objeto
+
+$objetoAlumno->asignarNombre("Cesar"); // llamando un metodo
+
+echo $objetoAlumno->nombre; // imprimir una propiedad
+?>
