6fd346175b1d300aa136322cfa8ba71418f2ccea
cesarco <87877289+Pcesarco@users.noreply.github.com>
2021-12-12 10:54:40 -0500

    Herencia | PHP

---
+++ Ejercicio_25.php
@@ -0,0 +1,40 @@
+<?php
+
+class persona {
+
+    public $nombre; // propiedades
+
+    private $edad; // se puede utilizar desde la misma clase en que se creo o se definio
+
+    protected $altura; // se accede desde la misma clase y tambien de las clases heredadas
+
+    public function asignarNombre($nuevoNombre){ // acciones o metodos..
+
+            $this->nombre=$nuevoNombre;
+    }
+
+    public function imprimirNombre(){
+        echo " Hola soy ".$this->nombre;
+    }
+
+    public function mostrarEdad(){
+        $this->edad = 20;
+        return $this->edad;
+    }
+}
+
+class trabajador extends persona {
+    public $puesto; // Propiedad nueva
+    public function saludoTrabajador(){
+        echo "Hola soy ".$this->nombre." y soy un ".$this->puesto;
+    }
+}
+
+$objetoTrabajador= new trabajador(); // instacion p creacion de un objeto
+$objetoTrabajador->asignarNombre("Cesar R."); // llamando un metodo
+
+$objetoTrabajador->puesto = "Developer";
+$objetoTrabajador->saludoTrabajador();
+
+
+?>
