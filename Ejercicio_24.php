6e311add4954f6913a3befbc5e896ba595929c25
cesarco <87877289+Pcesarco@users.noreply.github.com>
2021-12-12 10:33:53 -0500

    clases | PHP

--- Ejercicio_24.php
+++ Ejercicio_24.php
@@ -8,12 +8,21 @@ class persona {
 
             $this->nombre=$nuevoNombre;
     }
+
+    public function imprimirNombre(){
+        echo " Hola soy ".$this->nombre;
+    }
 }
 
 $objetoAlumno = new persona(); // instacion p creacion de un objeto
 $objetoAlumno->asignarNombre("Cesar"); // llamando un metodo
 
+$objetoAlumno2 = new persona(); // instacion p creacion de un objeto
+$objetoAlumno2->asignarNombre("Maria");
+$objetoAlumno2->imprimirNombre(); // llamando un metodo
 
 
-echo $objetoAlumno->nombre; // imprimir una propiedad
+echo $objetoAlumno->nombre;
+echo $objetoAlumno2->nombre; // imprimir una propiedad
+ // imprimir una propiedad
 ?>
