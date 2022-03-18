3f220748b1381264b2dd5390b650c891dcd1309e
cesarco <87877289+Pcesarco@users.noreply.github.com>
2022-03-18 12:12:36 -0500

    se agregaron comentarios

--- Ejercicio1.php
+++ Ejercicio1.php
@@ -3,6 +3,7 @@
 // Instrucción de impresión de informacion
 
 echo "Hello world, from villavicencio";
+ // echo es una instrucción de PHP, que imprime texto
 
 /*
 Comentario largoDB::delete('delete users where name = ?', ['John'])
@@ -10,7 +11,8 @@ Este es un comentario de código
 */
 
 print_r(" hola mundo");
+// En cambio print_r, muestra mas y cualquier tipo de valor
 
-print("hola");
+print(" hola");
 
 ?>
