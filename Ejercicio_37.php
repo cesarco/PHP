23b734b7b8eb7f02593e0674b8fe2fcc990621f8
cesarco <87877289+Pcesarco@users.noreply.github.com>
2022-03-19 19:26:06 -0500

    Función REQUIRE

---
+++ Ejercicio_37.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SITIO WEB</title>
+</head>
+<body>
+    <?php require "Ejercicio_37_1.php"; ?>
+</body>
+</html>
