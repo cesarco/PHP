0eff0fec199e29c78dbc0a853cb521f748c80c03
cesarco <87877289+Pcesarco@users.noreply.github.com>
2022-03-19 19:39:32 -0500

    Abrir un archivo en php

--- Ejercicio_38.php
+++ Ejercicio_38.php
@@ -1,10 +1,10 @@
 <?php
-
+// abriendo un archivo para leer el contenido
 $archivo = "contenido.txt";
 
-$archivoAbierto = fopen($archivo,"r");
+$archivoAbierto = fopen($archivo,"r"); // abre el archivo
 
-$contenido = fread($archivoAbierto,filesize($archivo));
+$contenido = fread($archivoAbierto,filesize($archivo)); // lee el archivo
 
 echo $contenido;
 
